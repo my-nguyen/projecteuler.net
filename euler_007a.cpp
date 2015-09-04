51305622b415a01e761a98d860c974380907105f
iceguyvn <nguyen_my@yahoo.com>
2015-09-04 14:15:09 -0700

    Euler 7, C++: separate methods get_primes() and pack_primes() from class bitfield so that bitfield only does bit manipulation and nothing else.

--- euler_007a.cpp
+++ euler_007a.cpp
@@ -8,13 +8,9 @@ const int BITS_PER_LONG = (sizeof(long)*BITS_PER_BYTE);
 class bitfield
 {
 public:
-  bitfield(int size);
+  bitfield(int space);
 
   ~bitfield() { delete [] _field; }
-
-  void get_primes();
-
-  void pack_primes(int** primes, int* prime_count);
 
   void print(unsigned long field);
 
@@ -24,16 +20,18 @@ public:
   void set(int long_index)
   { _field[byte_index(long_index)] |= (1l << bit_index(long_index)); }
 
-  unsigned long test(int long_index)
+  unsigned long test(int long_index) const
   { return _field[byte_index(long_index)] & (1l << bit_index(long_index)); }
+
+  int space() const { return _space; }
 
 private:
   char* decimal2binary(long decimal, char binary[]);
-  int byte_index(int long_index) { return long_index / BITS_PER_LONG; }
-  int bit_index(int long_index) { return long_index % BITS_PER_LONG; }
+  int byte_index(int long_index) const { return long_index / BITS_PER_LONG; }
+  int bit_index(int long_index) const { return long_index % BITS_PER_LONG; }
 
   unsigned long* _field;
-  int _size;
+  int _space;
 };
 
 bitfield::bitfield(int space)
@@ -48,7 +46,7 @@ bitfield::bitfield(int space)
   // initialize all bits to ON
   memset(_field, 0xFF, long_count * sizeof(long));
 
-  _size = space;
+  _space = space;
 }
 
 // This method converts a long integer in decimal base to a binary string.
@@ -77,24 +75,24 @@ void bitfield::print(unsigned long field)
 // less than 1 second.
 // This method finds all prime numbers from 2 up to size and stores them in an
 // array, with prime numbers represented as ON bit and non-prime as OFF
-void bitfield::get_primes()
+void get_primes(bitfield* field)
 {
   // numbers 0 and 1 are not primes
-  clear(0);
-  clear(1);
+  field->clear(0);
+  field->clear(1);
 
   // start with 2, the very first prime number
-  for (int i = 2; i < _size; i++)
+  for (int i = 2; i < field->space(); i++)
   {
     // test whether bit at i is ON
-    if (test(i))
+    if (field->test(i))
     {
       int multiple = 2;
-      while (i*multiple <= _size)
+      while (i*multiple <= field->space())
       {
         // all multiples of a prime numbers (2p, 3p, 4p, etc) are not primes.
         // So turn off bit at (i*multiple)th position.
-        clear(i*multiple);
+        field->clear(i*multiple);
         multiple += 1;
       }
     }
@@ -103,14 +101,14 @@ void bitfield::get_primes()
 
 // this method converts an array of booleans, where ON elements represent prime
 // numbers and OFF otherwise, into an array of prime numbers
-void bitfield::pack_primes(int** primes, int* prime_count)
+void pack_primes(const bitfield& field, int** primes, int* prime_count)
 {
   // 105000 currently accommodates up to the biggest test prime number: 10001
   // so it covers all the smaller test prime numbers.
   int tmp[105000];
   *prime_count = 0;
-  for (int i = 0; i < _size; i++)
-    if (test(i))
+  for (int i = 0; i < field.space(); i++)
+    if (field.test(i))
     {
       tmp[*prime_count] = i;
       *prime_count += 1;
@@ -150,15 +148,14 @@ int main()
     // First, obtain all prime numbers up to space. The prime numbers are ON bits
     // while the non-prime numbers are OFF bits.
     bitfield field(targets[i].space);
-    field.get_primes();
+    get_primes(&field);
 
     // Next, extract those ON bits into another array of prime numbers
     int* primes;
     int prime_count;
-    field.pack_primes(&primes, &prime_count);
+    pack_primes(field, &primes, &prime_count);
 
     cout << "found " << prime_count << " prime numbers below " << targets[i].space << endl;
-    // array_print(primes, prime_count);
     cout << "the " << targets[i].prime << "th prime number is " << primes[targets[i].prime-1] << endl;
     cout << "**********************************************" << endl;
 
