b4bd3ac7321081956f2401ef05b6e3400aeeb2dd
iceguyvn <nguyen_my@yahoo.com>
2015-09-11 17:47:21 -0700

    Euler 12, Ruby, added illustrating comments

--- euler_012.rb
+++ euler_012.rb
@@ -45,14 +45,36 @@ def find_divisors(number)
   divisors
 end
 
+# divisors contains all the divisors/factors of a triangle, for example:
+# triangle  divisors
+#      1    1
+#      3    1,3
+#      6    1,2,3,6
+#     10    1,2,5,10
+#     15    1,3,5,15
+#     21    1,3,7,21
+#     28    1,2,4,7,14,28
 divisors = []
-triangles = [1]
+# let's start numbering at 2 instead of 1, which means triangles already contains
+# number 1
 number = 2
-limit = 1
-while limit < 500
-  while divisors.size <= limit
+triangles = [1]
+# in the beginning, target is set at 1, but as a target is reached, a new target
+# is set by adding 1 to the number of divisors of the current triangle. since
+# the number of divisors is an arbitrary number that is greater than the current
+# target, the target gets increased pretty fast as shown in the resulting number
+# of divisors:
+# 2, 4, 6, 9, 16, 18, 20, 24, 36, 40, 48, 90, 112, 128, 144, 162, 168, 192, 240,
+# 320, 480, 576.
+target = 1
+# eventual target
+while target < 500
+  while divisors.size <= target
+    # triangle is the sum of the previous entry and current number
     triangle = triangles[number-2] + number
+    # retain that sum
     triangles << triangle
+    # get all divisors for that sum
     divisors = find_divisors(triangle)
 =begin
     print("number: #{number}, triangle: #{triangle}=>")
@@ -63,5 +85,6 @@ while limit < 500
     number += 1
   end
   puts("First that reach #{divisors.size}: number #{number-1}, triangle #{triangle}")
-  limit = divisors.size + 1
+  # set a new target based on the number of divisors of the current triangle
+  target = divisors.size + 1
 end
