1337c4ac632dfb9ca595c6f31e32cc796b725bf3
iceguyvn <nguyen_my@yahoo.com>
2015-09-14 20:57:52 -0700

    Euler 16, Ruby complete submission

---
+++ euler_016.rb
@@ -0,0 +1,21 @@
+# Power digit sum
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# What is the sum of the digits of the number 2^1000?
+#
+
+exponents = [ 15, 50, 100, 250, 500, 750, 1000 ]
+
+exponents.each do |exponent|
+  # take 2 to the power of exponent. this will be a huge number as exponent gets
+  # bigger
+  power = 2**exponent
+
+  # convert the huge integer into string, break that string into an array of
+  # characters, then turn that array into an array of digits
+  numbers = power.to_s.chars.map(&:to_i);
+
+  # take the sum of all the digits in the array of digits
+  sum = numbers.inject(:+)
+
+  puts("2^#{exponent}: #{power}, sum: #{sum}")
+end
