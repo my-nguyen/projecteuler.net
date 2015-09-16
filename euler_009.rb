e63ed42f1f3761913e6ae3ed58f4b6f66a4b3418
iceguyvn <nguyen_my@yahoo.com>
2015-09-16 12:32:55 -0700

    Euler 9, moved description to separate text file

--- euler_009.rb
+++ euler_009.rb
@@ -1,10 +1,3 @@
-# Special Pythagorean triplet
-# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-# a**2 + b**2 = c**2
-# For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
-# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-# Find the product a*b*c.
-#
 # According to Wikipedia, Euclid's formula is a fundamental formula for
 # generating Pythagorean triples given an arbitrary pair of positive integers
 # m and n with m > n. The formula states that the integers
