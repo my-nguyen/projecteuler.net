518e380686f341bcbeb007fbf013433c0c0a7f04
iceguyvn <nguyen_my@yahoo.com>
2015-09-16 12:41:17 -0700

    Euler 15, moved description to separate text file

--- euler_015.rb
+++ euler_015.rb
@@ -1,8 +1,3 @@
-# Lattice paths
-# Starting in the top left corner of a 2×2 grid, and only being able to move to
-# the right and down, there are exactly 6 routes to the bottom right corner.
-# How many such routes are there through a 20×20 grid?
-#
 # As explained in
 # http://blog.functionalfun.net/2008/07/project-euler-problem-15-city-grids-and.html
 # the number of routes in a NxN grid is the value of the element located at row
