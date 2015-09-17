129c6dd0bbc14e0737a4e9bf9d6c3f544dd0764b
iceguyvn <nguyen_my@yahoo.com>
2015-09-17 12:21:13 -0700

    Euler 22, Ruby complete submission

---
+++ euler_022.rb
@@ -0,0 +1,61 @@
+# this method opens a file, reads the only line in the file which contains
+# over 5,000 names, parse the names, store them in a list, then return the list
+def input
+  names = []
+
+  # read from file.
+  File.open("euler_022-names.txt", "r") do |file|
+    # there's only one huge line (46447 characters) to read
+    file.each_line do |line|
+      # parse the line into strings (names) based on delimeter ",\""
+      # retain each name in a list
+      names = line.split(/\W+/)
+    end
+
+    # this is a hack to remove the first string (which is somehow empty) from
+    # array. the strings result from the split operation based on a regular
+    # expresion, so that regex is faulty.
+    names.shift
+    # sort the names alphabetically
+    names.sort!
+  end
+
+  names
+end
+
+# this method returns a list of scores of the corresponding names. each score
+# is calculated by taking the total of the alphabetical value of all letters
+# in a name before multiplying that total by the alphabetical position of
+# that name.
+def scores(names)
+  scores = []
+  # go thru the name list
+  names.each_index do |i|
+    sum = 0
+    names[i].each_char do |letter|
+      # sum of the alphabetical value of all letters of a name
+      value = letter.ord - 'A'.ord + 1
+      sum += value
+      # cout << "char: " << *jt << ", value: " << value << endl;
+    end
+    # cout << "total: " << sum << ", index: " << i << endl;
+    # multiply the sum with the alphabetical position of the name
+    sum *= i + 1
+
+    # retain that product in a list
+    scores << sum
+  end
+
+  scores
+end
+
+# this method returns the sum of all integers in a list
+def sum(list)
+  list.inject(:+)
+end
+
+names = input
+# vector<string>::iterator it = find(names.begin(), names.end(), "COLIN");
+# cout << "index of COLIN: " << distance(names.begin(), it) << endl;
+points = scores(names)
+puts("Total: #{sum(points)}")
