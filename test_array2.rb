7c1b3c5e839f3ab9f36e9cc0803d1b79433f7d3d
iceguyvn <nguyen_my@yahoo.com>
2015-10-02 15:45:55 -0700

    cleanup

--- test_array2.rb
+++
@@ -1,6 +0,0 @@
-array = Array.new
-array = [3, 4, 5]
-array.each do |number|
-  print "#{number} "
-end
-puts
--- test_sizeof.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-  printf("sizeof(char): %ld\n", sizeof(char));
-  printf("sizeof(short): %lu\n", sizeof(short));
-  printf("sizeof(int): %lu\n", sizeof(int));
-  printf("sizeof(long): %ld\n", sizeof(long));
-}
