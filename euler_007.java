2fdbfe923ab37447e3a224a3c48867321e3cb1fe
iceguyvn <nguyen_my@yahoo.com>
2015-10-07 17:29:35 -0700

    Euler 7 (Java, C++ and Ruby) refactored

--- euler_007.java
+++ euler_007.java
@@ -1,42 +1,163 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 class euler_007
 {
-  public static void main(String[] args)
+  static boolean worst_prime(int number)
   {
-    final class target_t
+    if (number == 2)
+      return true;
+    // only consider odd divisors from 3 up to half the test number
+    for (int i = 3; i < number/2; i += 2)
+      if (number % i == 0)
+        return false;
+    return true;
+  }
+
+  static int worst_index(int position)
+  {
+    // only consider numbers greater than 3
+    int result = 3;
+    // first two prime numbers (2 and 3) are already accounted for
+    int incr = 2;
+    while (incr < position)
     {
-      target_t(int p, int s)
+      // only consider odd numbers
+      result += 2;
+      if (worst_prime(result))
+        incr += 1;
+    }
+    return result;
+  }
+
+  // Sieve of Eratosthenes algorithm (from wikipedia) to find all prime numbers
+  // up to a target number. This algorithm proves to be a vast improvement on my
+  // older prime-finding algorithm: while the older version took 13 seconds,
+  // this new version took less than 1 second.
+  // This constructor fills an array of bools from 2 to order, with prime
+  // numbers marked as ON bit and non-prime as OFF
+  static BitSet sieve_init(int position)
+  {
+    // the 100th prime is 541, the 1000th prime is 7919, the 10000th prime is
+    // 104729, the 100000th prime is 1299709 so size is roughly 13 times the
+    // prime number/position
+    int size = position * 13;
+    BitSet field = new BitSet(size);
+
+    // initialize all bits to ON
+    field.set(0, size);
+
+    // numbers 0 and 1 are not primes
+    field.clear(0);
+    field.clear(1);
+
+    // start with 2, the very first prime number
+    for (int i = 2; i < size; i++)
+    {
+      // test whether bit at i is ON
+      if (field.get(i))
       {
-        prime = p;
-        space = s;
+        int multiple = 2;
+        while (i*multiple < size)
+        {
+          // all multiples of a prime numbers (2p, 3p, 4p, etc) are not primes.
+          // So turn off bit at (i*multiple)th position.
+          field.clear(i*multiple);
+          multiple += 1;
+        }
+      }
+    }
+    return field;
+  }
+
+  // this method returns the natural number of the prime number at position.
+  // for example, for prime at position 100, it returns 541. for prime at 1000,
+  // it returns 7919. for prime at 5000, it returns 48611.
+  static int sieve_index(int position)
+  {
+    BitSet field = sieve_init(position);
+    // index to the entire bool array
+    int result = 1;
+    // index by prime numbers
+    int j = 1;
+    while (j < position)
+    {
+      result += 2;
+      if (field.get(result))
+        j += 1;
+    }
+    return result;
+  }
+
+  static boolean sieve2_prime(int number)
+  {
+    if (number == 2)
+      return true;
+    if (number % 2 == 0)
+      return false;
+    int i = 3;
+    while (i*i <= number)
+    {
+      if (number % i == 0)
+        return false;
+      i += 2;
+    }
+    return true;
+  }
+
+  static int sieve2_index(int position)
+  {
+    int result = 1;
+    int j = 1;
+    while (j < position)
+    {
+      result += 2;
+      if (sieve2_prime(result))
+        j += 1;
+    }
+    return result;
+  }
+
+  static int sieve3_index(int position)
+  {
+    List<Integer> primes = new ArrayList<>();
+    primes.add(2);
+    int result = 1;
+    while (primes.size() < position)
+    {
+      // only consider odd numbers
+      result += 2;
+      boolean found = true;
+      // result = 3, primes = [2], j = 0, primes[0]*primes[0] > 3 ==> prime
+      // result = 5, primes = [2, 3], j = 0, result%primes[0] == 1
+      //                              j = 1, primes[1]*primes[1] > 5 ==> prime
+      // result = 7, primes = [2, 3, 5], j = 0, result%primes[0] == 1
+      //                                 j = 1, primes[1]*primes[1] > 7 ==> prime
+      // result = 9, primes = [2, 3, 5, 7], j = 0, result%primes[0] == 1
+      //                                    j = 1, result%primes[1] == 0 ==> not prime
+      int j = 0;
+      while (found && j < primes.size() && primes.get(j)*primes.get(j) <= result)
+      {
+        if (result % primes.get(j) == 0)
+          found = false;
+        j += 1;
       }
 
-      int prime; // which prime number to get to
-      int space; // how much space is needed to cover the prime number
-    };
-
-    List<target_t> targets = new ArrayList<target_t>();
-    targets.add(new target_t(6, 20));
-    targets.add(new target_t(11, 50));
-    targets.add(new target_t(101, 600));
-    targets.add(new target_t(1001, 8100));
-    targets.add(new target_t(2001, 17500));
-    targets.add(new target_t(3001, 27600));
-    targets.add(new target_t(4001, 38000));
-    targets.add(new target_t(5001, 48800));
-    targets.add(new target_t(6001, 59500));
-    targets.add(new target_t(7001, 70800));
-    targets.add(new target_t(8001, 82000));
-    targets.add(new target_t(9001, 93300));
-    targets.add(new target_t(10001, 105000));
-
-    for (target_t target : targets)
-    {
-      primes myprimes = new primes(target.space);
-
-      System.out.println("prime(" + target.prime + "): " + myprimes.index(target.prime));
+      if (found)
+        primes.add(result);
     }
+    return result;
+  }
+
+  public static void main(String[] args)
+  {
+    int[] targets = { 6, 11, 101, 1001, 10001, 50001 };
+    for (int target : targets)
+      System.out.println("Worst(" + target + "): " + worst_index(target));
+    for (int target : targets)
+      System.out.println("Sieve(" + target + "): " + sieve_index(target));
+    for (int target : targets)
+      System.out.println("Modified Sieve(" + target + "): " + sieve2_index(target));
+    for (int target : targets)
+      System.out.println("Improved Sieve(" + target + "): " + sieve3_index(target));
   }
 }
--- euler_007.rb
+++ euler_007.rb
@@ -1,11 +1,150 @@
-require_relative 'primes'
+def worst_prime(number)
+  if (number == 2)
+    return true
+  end
+  # only consider odd divisors from 3 up to half the test number
+  (3..(number/2)).step(2) do |i|
+    if (number % i == 0)
+      return false
+    end
+  end
+  true
+end
 
-# reverse engineer: since I already solved this problem using a not-so-efficient
-# algorithm to find primes (which took 13 seconds), I know the answer to be
-# 104743, so I just have to declare array size as 105000
-SIZE = 105000
-primes = Primes.new(SIZE)
-targets = [6, 11, 101, 1001, 2001, 3001, 4001, 5001, 6001, 7001, 8001, 9001, 10001]
+def worst_index(position)
+  # only consider numbers greater than 3
+  result = 3
+  # first two prime numbers (2 and 3) are already accounted for
+  incr = 2
+  while (incr < position)
+    # only consider odd numbers
+    result += 2
+    if (worst_prime(result))
+      incr += 1
+    end
+  end
+  result
+end
+
+# Sieve of Eratosthenes algorithm (from wikipedia) to find all prime numbers
+# up to a target number. This algorithm proves to be a vast improvement on my
+# older prime-finding algorithm: while the older version took 13 seconds,
+# this new version took less than 1 second.
+# This constructor fills an array of bools from 2 to order, with prime
+# numbers marked as ON bit and non-prime as OFF
+def sieve_init(position)
+  # the 100th prime is 541, the 1000th prime is 7919, the 10000th prime is
+  # 104729, the 100000th prime is 1299709 so size is roughly 13 times the
+  # prime number/position
+  # initialize all bits to ON
+  field = Array.new(position*13, true)
+
+  # numbers 0 and 1 are not primes
+  field[0] = false
+  field[1] = false
+  # start with 2, the very first prime number
+  2.upto(position*13) do |i|
+    # test whether bit at i is ON
+    if (field[i])
+      multiple = 2
+      while (i*multiple < field.size)
+        # all multiples of a prime numbers (2p, 3p, 4p, etc) are not primes.
+        # So turn off bit at (i*multiple)th position.
+        field[i*multiple] = false
+        multiple += 1
+      end
+    end
+  end
+  field
+end
+
+# this method returns the natural number of the prime number at position.
+# for example, for prime at position 100, it returns 541. for prime at 1000,
+# it returns 7919. for prime at 5000, it returns 48611.
+def sieve_index(position)
+  field = sieve_init(position)
+  # index to the entire bool array
+  result = 1
+  # index by prime numbers
+  j = 1
+  while (j < position)
+    result += 2
+    if (field[result])
+      j += 1
+    end
+  end
+  result
+end
+
+def sieve2_prime(number)
+  if (number == 2)
+    return true
+  end
+  if (number % 2 == 0)
+    return false
+  end
+  i = 3
+  while (i*i <= number)
+    if (number % i == 0)
+      return false
+    end
+    i += 2
+  end
+  true
+end
+
+def sieve2_index(position)
+  result = 1
+  j = 1
+  while (j < position)
+    result += 2
+    if (sieve2_prime(result))
+      j += 1
+    end
+  end
+  result
+end
+
+def sieve3_index(position)
+  primes = []
+  primes << 2
+  result = 1
+  while (primes.size < position)
+    # only consider odd numbers
+    result += 2
+    found = true
+    # result = 3, primes = [2], j = 0, primes[0]*primes[0] > 3 ==> prime
+    # result = 5, primes = [2, 3], j = 0, result%primes[0] == 1
+    #                              j = 1, primes[1]*primes[1] > 5 ==> prime
+    # result = 7, primes = [2, 3, 5], j = 0, result%primes[0] == 1
+    #                                 j = 1, primes[1]*primes[1] > 7 ==> prime
+    # result = 9, primes = [2, 3, 5, 7], j = 0, result%primes[0] == 1
+    #                                    j = 1, result%primes[1] == 0 ==> not prime
+    j = 0
+    while (found && j < primes.size && primes[j]*primes[j] <= result)
+      if (result % primes[j] == 0)
+        found = false
+      end
+      j += 1
+    end
+
+    if (found)
+      primes << result
+    end
+  end
+  result
+end
+
+targets = [ 6, 11, 101, 1001, 10001, 50001 ]
 targets.each do |target|
-  puts("prime(#{target}): #{primes.index(target)}")
+  puts("Worst(#{target}): #{worst_index(target)}")
+end
+targets.each do |target|
+  puts("Sieve(#{target}): #{sieve_index(target)}")
+end
+targets.each do |target|
+  puts("Modified Sieve(#{target}): #{sieve2_index(target)}")
+end
+targets.each do |target|
+  puts("Improved Sieve(#{target}): #{sieve3_index(target)}")
 end
