79887f31408cadc22d82776425b5a716fe78e9e3
iceguyvn <nguyen_my@yahoo.com>
2015-10-12 17:18:24 -0700

    Euler 21 (Java and Txt), cosmetic changes

--- euler_021.java
+++ euler_021.java
@@ -48,11 +48,11 @@ public class euler_021
     {
       // take the sum of all divisors of the current number (i)
       int sum = sum(divisors(i));
-      // improvement: only need to check for a pair if sum is greater than i
       // at this point in the previous version, in order to look for a matching
       // pair, there was an inner loop going from 1 to 10000. as a result, the
       // old program took 95,626 milliseconds; compare that with the 89 ms it
-      // took in the improved version.
+      // takes in the improved version.
+      // improvement: only need to check for a pair if sum is greater than i.
       if (sum > i)
       {
         // take the sum of all divisors of the sum, and see if that equals the
