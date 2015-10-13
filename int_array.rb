ac0a345e36babf45b88aa7f092ef2302c38dfed7
iceguyvn <nguyen_my@yahoo.com>
2015-10-12 17:38:33 -0700

    Removed surplus files

--- int_array.rb
+++
@@ -1,9 +0,0 @@
-module IntArray
-  def output numbers
-    numbers.each_index do |i|
-      print " #{numbers[i]}"
-      puts if ((i+1) % 10 == 0 && i != (numbers.size-1))
-    end
-    puts
-  end
-end
